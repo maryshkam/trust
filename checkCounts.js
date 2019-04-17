f1aca07cc71255f1f80647063d03556c57411382
Mariia Gushuley <maryshka.moroz@gmail.com>
2019-04-17 15:25:14 +0300

    add checkCount.js

---
+++ checkCounts.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const checkCounts = function (value) {
+  let counter = 0;
+  const odd = (number) => 3 * number + 1;
+  const even = (number) => number/2;
+  
+  do {
+    if(value % 2 !== 0) {
+      value = odd (value);
+      } else {
+      value = even(value)
+      }
+    counter += 1;
+  }while(value > 1)
+  console.log(counter)
+}
+checkCounts(23061912); // 71
